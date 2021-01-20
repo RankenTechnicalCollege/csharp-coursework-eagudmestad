1af198ffb3902e12d8afd9eb02b229b7bc2c4a8c
Evan Gudmestad <eagudmestad@ranken.edu>
2021-01-20 08:19:58 -0600

    Added .gitignore plus a couple of demo labs

---
+++ Lab/CH2/LB4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LB4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name:");
+            string firstName = Console.ReadLine();//"Evan";
+
+
+            // Console.WriteLine("{0}", firstName.Length);
+            //Length property holds the number of characters in the string
+
+            string firstInitial = firstName.Substring(0, 1);
+            string restOfName = firstName.Substring(1, firstName.Length-1);
+            Console.WriteLine("Hello {0}{1}ay", restOfName, firstInitial);
+        }
+    }
+}
